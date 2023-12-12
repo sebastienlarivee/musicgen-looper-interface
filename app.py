5e7f7e1526672a0704e7bc0bb423b06467fedeb3
sebastienlarivee <lariveesebastien@gmail.com>
2023-12-12 10:51:28 -0800

    Interface tweaks

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import gradio as gr
-import requests
+import replicate
 
 
 def api_call(
-    bpm, seed, prompt, variations, temperature, model_version, output_format, guidance
+    bpm,
+    seed,
+    prompt,
+    variations,
+    temperature,
+    max_duration,
+    model_version,
+    output_format,
+    guidance,
 ):
     # Prepare the API request
     api_request_payload = {
@@ -14,44 +22,48 @@ def api_call(
         "prompt": prompt,
         "variations": variations,
         "temperature": temperature,
-        "max_duration": 10,
+        "max_duration": max_duration,
         "model_version": model_version,
         "output_format": output_format,
         "classifier_free_guidance": guidance,
     }
 
-    # Make the API call
-    response = requests.post(
-        "API_ENDPOINT",  # Replace with the actual API endpoint
-        json={"input": api_request_payload},
+    output = replicate.run(
+        "andreasjansson/musicgen-looper:f8140d0457c2b39ad8728a80736fea9a67a0ec0cd37b35f40b68cce507db2366",
+        input=api_request_payload,
     )
-
-    # Parse the response for audio file links
-    # audio_links = parse_response(response)  # Define the parse_response function as needed
-
-    # Download audio files
-    # download_audio_files(audio_links)  # Define the download_audio_files function as needed
-
-    return "Audio files downloaded successfully."
+    return output
 
 
 with gr.Blocks() as demo:
     with gr.Row():
-        bpm_slider = gr.Slider(minimum=50, maximum=250, label="BPM")
-        seed_input = gr.Textbox(label="Seed")
-    with gr.Row():
         prompt_input = gr.Textbox(label="Prompt")
-        variations_slider = gr.Slider(minimum=1, maximum=10, label="Variations")
+
     with gr.Row():
-        temperature_slider = gr.Slider(minimum=0, maximum=1, label="Temperature")
-        model_version_toggle = gr.Radio(
-            choices=["medium", "large"], label="Model Version"
+        bpm_slider = gr.Slider(minimum=50, maximum=250, value=100, label="BPM")
+        max_duration_slider = gr.Slider(
+            minimum=5, maximum=30, value=10, label="Max Duration"
+        )
+        variations_slider = gr.Slider(
+            minimum=1, maximum=10, value=1, label="Variations"
         )
     with gr.Row():
-        output_format_toggle = gr.Radio(choices=["wav", "mp3"], label="Output Format")
+        seed_input = gr.Textbox(value=-1, label="Seed")
+        temperature_slider = gr.Slider(
+            minimum=0, maximum=1, value=1, label="Temperature"
+        )
         guidance_slider = gr.Slider(
-            minimum=0, maximum=20, label="Classifier Free Guidance"
+            minimum=0, maximum=15, value=3, label="Classifier Free Guidance"
         )
+
+    with gr.Row():
+        output_format_toggle = gr.Radio(
+            choices=["wav", "mp3"], value="wav", label="Output Format"
+        )
+        model_version_toggle = gr.Radio(
+            choices=["medium", "large"], value="large", label="Model Version"
+        )
+
     submit_button = gr.Button("Submit")
     output_label = gr.Label()
 
@@ -63,6 +75,7 @@ with gr.Blocks() as demo:
             prompt_input,
             variations_slider,
             temperature_slider,
+            max_duration_slider,
             model_version_toggle,
             output_format_toggle,
             guidance_slider,
