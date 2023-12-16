c0b207737cb0975f40e05b8da5304fca82dea41a
sebastienlarivee <lariveesebastien@gmail.com>
2023-12-15 16:39:54 -0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -190,7 +190,9 @@ class Generate:
         prompt_beats = 4  # placeholder, make variable?
         prompt_duration = (60 / self.bpm) * prompt_beats
         print(f"prompt_duration: {prompt_duration}")
-        self.duration = prompt_duration + self.audio_prompt / self.prompt_sample_rate
+        self.duration = (
+            prompt_duration + self.audio_prompt.size(1) / self.prompt_sample_rate
+        )
         print(f"self.durations: {self.duration}")
         beat_prompt = self.audio_prompt[
             ..., -int(prompt_duration * self.prompt_sample_rate) :
