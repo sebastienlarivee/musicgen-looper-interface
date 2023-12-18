cd1b7a3a0c963cde3556037297ff172339130f0e
sebastienlarivee <lariveesebastien@gmail.com>
2023-12-18 11:07:25 -0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -194,16 +194,17 @@ class Generate:
 
     def loop_generate_from_audio(self, name):
         # GRADIO ENDPOINT: Generate seamless loops from an audio prompt (prompt must also be a loop to work well)
+
+        # Select last 4 beats of input audio for use as audio prompt
         prompt_beats = 4  # placeholder, make it a parameter?
         prompt_seconds = (60 / self.bpm) * prompt_beats
-        input_loop_seconds = self.audio_prompt.size(1) / self.prompt_sample_rate
-        self.duration = prompt_seconds + input_loop_seconds + 0.1
-
         prompt = self.audio_prompt[
             ..., -int(prompt_seconds * self.prompt_sample_rate) :
         ]
 
-        # Need to reset gen params as we changed duration
+        # Calculate total duration (+ a bit of extra) and update generation params
+        input_loop_seconds = self.audio_prompt.size(1) / self.prompt_sample_rate
+        self.duration = prompt_seconds + input_loop_seconds + 0.1
         self.set_generation_params()
         print(f"self.duration: {self.duration}")
         print(f"input_loop_seconds: {input_loop_seconds}")
@@ -213,10 +214,10 @@ class Generate:
 
         # Slice generated audio from the end of the prompt to the length of the original loop
         # SOMETHING IS NOT RIGHT HERE
-        start_indice = int(input_loop_seconds * self.model_sample_rate)
-        end_indice = int(start_indice + prompt_seconds * self.model_sample_rate)
+        start_indice = int(prompt_seconds * self.model_sample_rate)
+        end_indice = int(start_indice + input_loop_seconds * self.model_sample_rate)
         print(
-            f"start_indice: {start_indice}, end_indice: {end_indice}, total samples: {end_indice-start_indice}, total seconds: {(end_indice-start_indice)/self.model_sample_rate}"
+            f"start_indice: {start_indice}, end_indice: {end_indice}, total samples: {end_indice-start_indice}, total output seconds: {(end_indice-start_indice)/self.model_sample_rate}"
         )
 
         wav = self.generate_from_audio(
